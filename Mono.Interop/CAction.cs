d8aa965779a7c37b7a6f98ad9a0543c70372bfde
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-04-19 18:44:00 +0200

    Add all generic argument overloads for func and action.

--- Mono.Interop/CAction.cs
+++ Mono.Interop/CAction.cs
@@ -308,5 +308,89 @@ namespace Mono.Interop
 			Dispose();
 		}
 	}
+
+	public class CAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> : CActionBase
+	{
+		public Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> Callback { get; protected set; }
+
+		Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> cb;
+
+		public CAction(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> callback)
+		{
+			cb = callback;
+		}
+
+		void PrivateCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13)
+		{
+			if (cb != null) {
+				cb(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13);
+			}
+
+			Dispose();
+		}
+	}
+
+	public class CAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> : CActionBase
+	{
+		public Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> Callback { get; protected set; }
+
+		Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> cb;
+
+		public CAction(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> callback)
+		{
+			cb = callback;
+		}
+
+		void PrivateCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14)
+		{
+			if (cb != null) {
+				cb(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14);
+			}
+
+			Dispose();
+		}
+	}
+
+	public class CAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> : CActionBase
+	{
+		public Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> Callback { get; protected set; }
+
+		Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> cb;
+
+		public CAction(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15> callback)
+		{
+			cb = callback;
+		}
+
+		void PrivateCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15)
+		{
+			if (cb != null) {
+				cb(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15);
+			}
+
+			Dispose();
+		}
+	}
+
+	public class CAction<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> : CActionBase
+	{
+		public Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> Callback { get; protected set; }
+
+		Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> cb;
+
+		public CAction(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16> callback)
+		{
+			cb = callback;
+		}
+
+		void PrivateCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15, T16 arg16)
+		{
+			if (cb != null) {
+				cb(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15, arg16);
+			}
+
+			Dispose();
+		}
+	}
 }
 
--- Mono.Interop/CFunc.cs
+++ Mono.Interop/CFunc.cs
@@ -200,5 +200,205 @@ namespace Mono.Interop
 			return ret;
 		}
 	}
+
+	public class CFunc<T1, T2, T3, T4, T5, T6, T7, T8, T> : CActionBase
+	{
+		public Func<T1, T2, T3, T4, T5, T6, T7, T8, T> Callback { get; protected set; }
+
+		Func<T1, T2, T3, T4, T5, T6, T7, T8, T> func;
+
+		public CFunc(Func<T1, T2, T3, T4, T5, T6, T7, T8, T> callback)
+		{
+			Callback = OnCallback;
+			func = callback;
+		}
+
+		public T OnCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8)
+		{
+			T ret;
+			if (func != null) {
+				ret = func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8);
+			} else {
+				ret = default(T);
+			}
+			Dispose();
+			return ret;
+		}
+	}
+
+	public class CFunc<T1, T2, T3, T4, T5, T6, T7, T8, T9, T> : CActionBase
+	{
+		public Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T> Callback { get; protected set; }
+
+		Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T> func;
+
+		public CFunc(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T> callback)
+		{
+			Callback = OnCallback;
+			func = callback;
+		}
+
+		public T OnCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9)
+		{
+			T ret;
+			if (func != null) {
+				ret = func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9);
+			} else {
+				ret = default(T);
+			}
+			Dispose();
+			return ret;
+		}
+	}
+
+	public class CFunc<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T> : CActionBase
+	{
+		public Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T> Callback { get; protected set; }
+
+		Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T> func;
+
+		public CFunc(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T> callback)
+		{
+			Callback = OnCallback;
+			func = callback;
+		}
+
+		public T OnCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10)
+		{
+			T ret;
+			if (func != null) {
+				ret = func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10);
+			} else {
+				ret = default(T);
+			}
+			Dispose();
+			return ret;
+		}
+	}
+
+	public class CFunc<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T> : CActionBase
+	{
+		public Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T> Callback { get; protected set; }
+
+		Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T> func;
+
+		public CFunc(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T> callback)
+		{
+			Callback = OnCallback;
+			func = callback;
+		}
+
+		public T OnCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11)
+		{
+			T ret;
+			if (func != null) {
+				ret = func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11);
+			} else {
+				ret = default(T);
+			}
+			Dispose();
+			return ret;
+		}
+	}
+
+	public class CFunc<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T> : CActionBase
+	{
+		public Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T> Callback { get; protected set; }
+
+		Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T> func;
+
+		public CFunc(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T> callback)
+		{
+			Callback = OnCallback;
+			func = callback;
+		}
+
+		public T OnCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12)
+		{
+			T ret;
+			if (func != null) {
+				ret = func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12);
+			} else {
+				ret = default(T);
+			}
+			Dispose();
+			return ret;
+		}
+	}
+
+	public class CFunc<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T> : CActionBase
+	{
+		public Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T> Callback { get; protected set; }
+
+		Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T> func;
+
+		public CFunc(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T> callback)
+		{
+			Callback = OnCallback;
+			func = callback;
+		}
+
+		public T OnCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13)
+		{
+			T ret;
+			if (func != null) {
+				ret = func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13);
+			} else {
+				ret = default(T);
+			}
+			Dispose();
+			return ret;
+		}
+	}
+
+	public class CFunc<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T> : CActionBase
+	{
+		public Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T> Callback { get; protected set; }
+
+		Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T> func;
+
+		public CFunc(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T> callback)
+		{
+			Callback = OnCallback;
+			func = callback;
+		}
+
+		public T OnCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14)
+		{
+			T ret;
+			if (func != null) {
+				ret = func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14);
+			} else {
+				ret = default(T);
+			}
+			Dispose();
+			return ret;
+		}
+	}
+
+	public class CFunc<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T> : CActionBase
+	{
+		public Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T> Callback { get; protected set; }
+
+		Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T> func;
+
+		public CFunc(Func<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T> callback)
+		{
+			Callback = OnCallback;
+			func = callback;
+		}
+
+		public T OnCallback(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6, T7 arg7, T8 arg8, T9 arg9, T10 arg10, T11 arg11, T12 arg12, T13 arg13, T14 arg14, T15 arg15)
+		{
+			T ret;
+			if (func != null) {
+				ret = func(arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14, arg15);
+			} else {
+				ret = default(T);
+			}
+			Dispose();
+			return ret;
+		}
+	}
 }
 
